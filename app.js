2b33e79aae66a8cc708d664205f2562e8ad43a59
Developer <comps381f@ouhk.edu.hk>
2017-12-04 11:15:01 +0800

    first commit

--- app.js
+++ app.js
@@ -1,38 +1,29 @@
-// get all required items
 
-var MongoClient = require('mongodb').MongoClient
-var bodyParser = require('body-parser')
 var express = require('express')
 var engines = require('consolidate')
+var MongoClient = require('mongodb').MongoClient
+var bodyParser = require('body-parser')
 var assert = require('assert')
 var logger = require('morgan')
 var path = require('path')
 var favicon = require('serve-favicon')
 var cookieSession = require('cookie-session')
 var fileUpload = require('express-fileupload')
-//var port = process.env.PORT || 1994
-
+var port = process.env.PORT || 1994
 var mongoUri = process.env.MONGOLAB_URI ||
      process.env.MONGOHQ_URL ||    
      'mongodb://my381f:1994211@ds125016.mlab.com:25016/my381project'
-
 var ObjectID = require('mongodb').ObjectID
 
 var app = express()
 global.userSet = new Set()
 global.rootPath = __dirname
 
-app.listen(process.env.PORT || 1994, function () {
-        console.log('Server listening on port 1994')
-    }  )
-
 app.use(cookieSession({
     name: 'session',
     keys: ['s381f'],
-    // Cookie Options
-    maxAge: 60*1000*2 // 2 minutes
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
 }))
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
@@ -43,23 +34,18 @@ app.use(bodyParser.urlencoded({
     limit: '50mb',
     extended: true
 }))
-app.use(express.static(__dirname + '/public'))
-
-
-
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(fileUpload({
     limits: {
         fileSize: 50 * 1024 * 1024
     },
 }))
-// make sure we can connect to database before starting server
+
 MongoClient.connect(mongoUri, function (err, db) {
     global.rt = db.collection('restaurants')
     assert.equal(null, err)
     global.user = db.collection('users')
-    console.log('Successfully connected to mongodb')
+    console.log('Successfully connected to mondodb')
     const restaurantUrl = '/restaurant'
     const apiUrl = '/api'
 
@@ -102,21 +88,13 @@ MongoClient.connect(mongoUri, function (err, db) {
                     resolve(data)
                 })
             }).then(function (data) {
-                try{
-                    if (data.length > 0) throw ('duplicate user')
-                    return global.user.insert({
+                if (data.length > 0) throw ('duplicate user')
+                return global.user.insert({
                     username,
                     password
                 })
-                }catch(err){
-                    res.send(alertMsg('Fail to register - ' + err))
-                }
-
-                
-                
             }).then(function () {
-                 res.send('<script>alert("Register Success");window.history.back()</script>')
-                
+                res.send('<script>alert("Register Success");window.history.back()</script>')
             })
         } catch (err) {
             res.send(alertMsg('Fail to register - ' + err))
@@ -124,7 +102,6 @@ MongoClient.connect(mongoUri, function (err, db) {
     })
 
     app.get('/logout', function (req, res) {
-        
         global.userSet.delete(req.session.username)
         res.clearCookie("session")
         res.clearCookie("session.sig")
@@ -196,7 +173,7 @@ MongoClient.connect(mongoUri, function (err, db) {
     app.post(restaurantUrl + '/edit/:id', function (req, res) {
         authen(req, res, function () {
             let id = req.params.id
-            let formData = getFormData(req)
+            let formData = getFormData(req, false, res)
             try {
                 const options1 = {
                     _id: new ObjectID(id)
@@ -238,39 +215,34 @@ MongoClient.connect(mongoUri, function (err, db) {
         })
     })
 
-    //api to read
     app.get(apiUrl + restaurantUrl + '/read/:param1/:param2', function (req, res) {
-        authen(req, res, function () {
-            try {
-                var param1 = req.params.param1
-                var param2 = req.params.param2
-                if ((['name', 'borough', 'cuisine'].indexOf(param1)) < 0) return res.sendStatus(404)
-                var options = {
-                    [param1]: param2
-                }
-                global.rt.find(options).toArray(function (err, data) {
-                    res.send(data)
-                })
-            } catch (err) {
-                res.send('Got error in getAll')
+        try {
+            var param1 = req.params.param1
+            var param2 = req.params.param2
+            if ((['name', 'borough', 'cuisine'].indexOf(param1)) < 0) return res.sendStatus(404)
+            var options = {
+                [param1]: param2
             }
-        })
+            global.rt.find(options).toArray(function (err, data) {
+                res.send(data)
+            })
+        } catch (err) {
+            res.send('Got error in getAll')
+        }
     })
 
     app.post(apiUrl + restaurantUrl + '/create', function (req, res) {
-        authen(req, res, function () {
-            new Promise(function (resolve, reject) {
-                addRtFlow(req, resolve, reject)
-            }).then((data) => {
-                res.send({
-                    status: 'ok',
-                    _id: data.insertedIds[0]
-                })
-            }).catch(function (err) {
-                res.send({
-                    status: 'failed',
-                    message: 'Error in getting restaurants - ' + err
-                })
+        new Promise(function (resolve, reject) {
+            addRtFlow(req, resolve, reject, true, res)
+        }).then((data) => {
+            res.send({
+                status: 'ok',
+                _id: data.insertedIds[0]
+            })
+        }).catch(function (err) {
+            res.send({
+                status: 'failed',
+                message: 'Error in getting restaurants - ' + err
             })
         })
     })
@@ -278,7 +250,7 @@ MongoClient.connect(mongoUri, function (err, db) {
     app.post(restaurantUrl, function (req, res) {
         authen(req, res, function () {
             new Promise(function (resolve, reject) {
-                addRtFlow(req, resolve, reject)
+                addRtFlow(req, resolve, reject, false, res)
             }).then(function (data) {
                 res.redirect('/restaurant/display/' + data.ops[0]._id)
             }).catch(function (err) {
@@ -300,24 +272,28 @@ MongoClient.connect(mongoUri, function (err, db) {
             }).then(function (data) {
                 return data[0].grades.map(function (grade) {
                     if (grade.user == req.session.username) {
-                        try {
-                            throw ('You have already rated the restaurant')
-                        } catch (err) {
-                            res.send(alertMsg('Failed to rate restaurant- ' + err))
-                        }
+                        return global.rt.update({
+                            _id: new ObjectID(id)
+                        }, {
+                                $pull: {
+                                    grades: {
+                                        user: req.session.username
+                                    }
+                                }
+                            })
                     }
                 })
             }).then(function (data) {
                 return global.rt.update({
                     _id: new ObjectID(id)
                 }, {
-                    $push: {
-                        grades: {
-                            user: req.session.username,
-                            score: rate
+                        $push: {
+                            grades: {
+                                user: req.session.username,
+                                score: rate
+                            }
                         }
-                    }
-                })
+                    })
             }).then(function () {
                 res.redirect('/restaurant/display/' + id)
             })
@@ -329,15 +305,15 @@ MongoClient.connect(mongoUri, function (err, db) {
         authen(req, res, function () {
             let id = req.params.id
             const owner = req.session.username
-                id = new ObjectID(id)
-                new Promise(function (resolve, reject) {
-                    global.rt.find({
-                        _id: id
-                    }).limit(1).toArray(function (err, data) {
-                        resolve(data)
-                    })
-                }).then(function (data) {
-            try {                
+            id = new ObjectID(id)
+            new Promise(function (resolve, reject) {
+                global.rt.find({
+                    _id: id
+                }).limit(1).toArray(function (err, data) {
+                    resolve(data)
+                })
+            }).then(function (data) {
+                try {
                     if (data[0].owner != owner) throw ('No Permission')
                     return global.rt.remove({
                         _id: id
@@ -345,15 +321,15 @@ MongoClient.connect(mongoUri, function (err, db) {
                 } catch (err) {
                     res.send(alertMsg('Failed to delete restaurant- ' + err))
                 }
-                }).then(function () {
-                    res.redirect('/restaurant')
-                })
-            
+            }).then(function () {
+                res.redirect('/restaurant')
+            })
+
         })
     })
 
-    function addRtFlow(req, resolve, reject) {
-        let formData = getFormData(req)
+    function addRtFlow(req, resolve, reject, isApi, res) {
+        let formData = getFormData(req, isApi, res)
         formData['grades'] = []
         new Promise(function (resolve, reject) {
             global.rt.find({
@@ -372,20 +348,43 @@ MongoClient.connect(mongoUri, function (err, db) {
         })
     }
 
-    function getFormData(req) {
-        // const restaurant_id = req.body.restaurant_id
-        const name = req.body.name
-        const cuisine = req.body.cuisine
-        const borough = req.body.borough
-        const street = req.body.street
-        const building = req.body.building
-        const zipcode = req.body.zipcode
-        const gpsLon = req.body.longtitude
-        const gpsLat = req.body.latitude
-        // const photo = req.body.photo
-        const owner = req.session.username || 'root'
+    function getFormData(req, isApi, res) {
         const errMsg = ' is not defined'
-        const photo = getPhoto(req)
+        let name = req.body.name
+        let cuisine = req.body.cuisine
+        let borough = req.body.borough
+        let street
+        let building
+        let zipcode
+        let gpsLon
+        let gpsLat
+        let owner
+        let photo
+        if (isApi) {
+            if (req.body.address) {
+                street = req.body.address.street || ''
+                building = req.body.address.building || ''
+                zipcode = req.body.address.zipcode || ''
+                gpsLon = req.body.address.coord.longtitude || ''
+                gpsLat = req.body.address.coord.latitude || ''
+            } else {
+                street = ''
+                building = ''
+                zipcode = ''
+                gpsLon = ''
+                gpsLat = ''
+            }
+            owner = req.body.owner || 'root'
+            photo = req.body.photo || {}
+        } else {
+            street = req.body.street
+            building = req.body.building
+            zipcode = req.body.zipcode
+            gpsLon = req.body.longtitude
+            gpsLat = req.body.latitude
+            owner = req.session.username || 'root'
+            photo = getPhoto(req, res)
+        }
         assert.notEqual(name, undefined, 'name' + errMsg)
         assert.notEqual(owner, undefined, 'owner' + errMsg)
 
@@ -409,12 +408,15 @@ MongoClient.connect(mongoUri, function (err, db) {
         return restaurantToAdd
     }
 
-    function getPhoto(req) {
+    function getPhoto(req, res) {
         var photo = {}
         if (req.files && req.files.rtPhoto) {
             photo = req.files.rtPhoto
             var fileName = photo.name,
                 type = photo.mimetype
+            if(!(type.includes('image'))) {
+                res.send(alertMsg('Please insert correct image file!'))
+            }
             const uploadPath = global.rootPath + '/public/images/' + req.body.name + '.' + type.replace('image/', '')
             photo.mv(uploadPath, function (err) {
                 if (err)
@@ -425,18 +427,10 @@ MongoClient.connect(mongoUri, function (err, db) {
         return photo
     }
 
-    // catch 404 and forward to error handler
     app.use(function (req, res, next) {
-        // var err = new Error('Not Found')
-        // err.status = 404
-        // next(err)
         res.redirect('/')
     })
 
-    // error handlers
-
-    // development error handler
-    // will print stacktrace
     if (app.get('env') === 'development') {
         app.use(function (err, req, res, next) {
             res.status(err.status || 500)
@@ -447,15 +441,12 @@ MongoClient.connect(mongoUri, function (err, db) {
         })
     }
 
-    // production error handler
-    // no stacktraces leaked to user
     app.use(function (err, req, res, next) {
         res.status(err.status || 500)
         res.render('error', {
             message: err.message,
             error: {}
         })
-        // res.redirect('/')
     })
 
     function authen(req, res, callback) {
@@ -470,14 +461,7 @@ MongoClient.connect(mongoUri, function (err, db) {
         return '<script>alert("' + msg + '");window.history.back()</script>'
     }
 
-    
-  
-
-    // app.listen(port, function () {
-    //     console.log('Server listening on port 1994')
-    // })
-
-    
-
-    
+    app.listen(port, function () {
+        console.log('Server listening on port 1994')
+    })
 })
